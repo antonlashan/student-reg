07a11c910f0c9064e9d77b245d5ed135023e770f
antonlashan2 <antonlashan@gmail.com>
2015-11-15 11:23:09 +0530

    frontpage layout

--- frontend/views/site/_search.php
+++ frontend/views/site/_search.php
@@ -10,20 +10,21 @@ use yii\widgets\ActiveForm;
 
 <div class="user-search">
 
-	<?php
-	$form = ActiveForm::begin([
-			'action' => ['index'],
-			'method' => 'get',
-	]);
-	?>
+    <?php
+    $form = ActiveForm::begin([
+                'action' => ['index'],
+                'method' => 'get',
+    ]);
+    ?>
+    <div class="row">
+        <div class="col-md-9">
+            <?= $form->field($model, 'search_e_n')->label(false) ?>
+        </div>
 
-	<?= $form->field($model, 'search_e_n')->label(false) ?>
-
-
-	<div class="form-group">
-		<?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
-	</div>
-
-	<?php ActiveForm::end(); ?>
+        <div class="col-md-3">
+            <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        </div>
+    </div>
+    <?php ActiveForm::end(); ?>
 
 </div>
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -10,42 +10,48 @@ use yii\widgets\ListView;
 
 $this->title = 'Home';
 ?>
-<div class="user-index">
-	<div class="jumbotron">
-		<h1>IPSL is the apex body of Physicists in Sri Lanka</h1>
-		<?= Html::img('@web/img/logo.png') ?>
-		<h2>Welcome to IPSL Membership Portal</h2>
-		<p class="lead">This Membership Portal contains services for members of the Institute of Physics, Sri Lanka. Membership is open to anyone with an interest in Physics, for more information please go to Membership.</p>
-		<p></p><br/>
-		<p>To enter the Membership Portal please sign in using registered email and password: </p>
-		<p><?= Html::a('Membership Login', ['login'], ['class' => 'btn btn-lg btn-primary']) ?></p>
-		<p>If you are a new member sign up today.</p>
-		<p><?= Html::a('Sign up today', ['signup'], ['class' => 'btn btn-lg btn-primary']) ?></p>
-	</div>
+<div class="site-index">
+    <div class="jumbotron">
+        <h1>Welcome to IPSL Membership Portal</h1>
+        <p style="margin-bottom: 0;">IPSL is the apex body of Physicists in Sri Lanka</p>
+        <?= Html::img('@web/img/logo.png',['width' => '150px']) ?>
+        <p class="lead">This Membership Portal contains services for members of the Institute of Physics, Sri Lanka. Membership is open to anyone with an interest in Physics, for more information please go to Membership.</p>
+        
+    </div>
 
-	<div class="body-content">
+    <div class="body-content">
+        <div class="row">
+            <div class="col-md-8">
+                <p class="lead text-center">To enter the Membership Portal please sign in using registered email and password: </p>
+                <p class="text-center"><?= Html::a('Membership Login', ['login'], ['class' => 'btn btn-lg btn-primary']) ?></p>
+            </div>
+            <div class="col-md-4">
+                <p class="lead text-center">If you are a new member sign up today.</p>
+                <p class="text-center"><?= Html::a('Sign up today', ['signup'], ['class' => 'btn btn-lg btn-primary']) ?></p>
+            </div>
+        </div>
+        <p></p>
+        <div class="row">
 
-		<div class="row">
-			
-			<div class="col-lg-7">
-				<h2>Search IPSL Members</h2>
+            <div class="col-md-9">
+                <span class="lead">Search IPSL Members</span>
 
-				<?php echo $this->render('_search', ['model' => $searchModel]); ?>
+                <?php echo $this->render('_search', ['model' => $searchModel]); ?>
 
-				<?php if ($dataProvider) { ?>
-					<?=
-					ListView::widget([
-						'dataProvider' => $dataProvider,
-						'itemOptions' => ['class' => 'item'],
-						'itemView' => function ($model, $key, $index, $widget) {
-						return Html::a(Html::encode($model->full_name), ['view-member', 'id' => $model->id]);
-					},
-					])
-					?>
-				<?php } ?>
-			</div>
-		</div>
+                <?php if ($dataProvider) { ?>
+                    <?=
+                    ListView::widget([
+                        'dataProvider' => $dataProvider,
+                        'itemOptions' => ['class' => 'item'],
+                        'itemView' => function ($model, $key, $index, $widget) {
+                    return Html::a(Html::encode($model->full_name), ['view-member', 'id' => $model->id]);
+                },
+                    ])
+                    ?>
+                <?php } ?>
+            </div>
+        </div>
 
-	</div>
+    </div>
 
 </div>
